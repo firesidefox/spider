bcb85ce2e16f1dd07c96462dd90e49b85c60538a
Chen Wei <xyzchw@aliyun.com>
2026-05-25 07:04:05 +0800

    refactor(frontend): rename /profile route to /setting

--- web/src/App.vue
+++ web/src/App.vue
@@ -9,7 +9,7 @@
         <RouterLink to="/knowledge" class="nav-item">知识库</RouterLink>
         <RouterLink to="/topology" class="nav-item">网络拓扑</RouterLink>
         <RouterLink to="/tasks" class="nav-item">任务管理</RouterLink>
-        <RouterLink to="/profile" class="nav-item">系统设置</RouterLink>
+        <RouterLink to="/setting" class="nav-item">系统设置</RouterLink>
       </nav>
       <div v-if="currentUser" class="nav-user">
         <div class="user-dropdown" @click.stop="showUserMenu = !showUserMenu">
